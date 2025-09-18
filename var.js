7dbcf1aacc056fcc8db6e5be89858cf3f5f52939
mbiplobmollah <biplobmollah16@gmail.com>
2025-09-18 23:07:19 +0600

    run time error and compile time eror

---
+++ var.js
@@ -0,0 +1,14 @@
+// console.log('Abu Rayhan', 'Abu Rayhan'.length);
+
+// throw new Error('Something went wrongR')
+
+
+// the above code is an example of runtime error
+
+
+
+// console.log('Abu Rayhan', 'Abu Rayhan'.length);
+
+// 1213215631515641g54g;
+
+// the above code is compile time error
