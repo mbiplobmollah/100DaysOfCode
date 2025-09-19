cd973fc4f02dcc39b5b1e6026c0054e2beee33e5
mbiplobmollah <biplobmollah16@gmail.com>
2025-09-19 22:14:00 +0600

    done with variable

--- var.js
+++ var.js
@@ -12,3 +12,36 @@
 // 1213215631515641g54g;
 
 // the above code is compile time error
+
+
+// console.log(x);  // undefined (no error)
+// var x = 10;
+// console.log(x);  // 10
+
+
+// the above is an example of var hoisting
+
+
+
+
+let names = [
+    'abu rayhan', 'shaker hossain', 'akib ahmad', 'alvi chowdhuri'
+];
+
+let index = -1 ; 
+
+let name = names[++index]
+
+
+setInterval(()=>{
+    name = names[index++];
+    console.log(name, name.length);
+
+
+    if(index === names.length){
+        index = 0;
+    }
+}, 1000)
+
+// console.log(name, name.length);
+
